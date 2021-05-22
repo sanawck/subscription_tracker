fd685f08b0e850d03f29532d6ca3a6d297dc0dfb
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-05-22 08:58:53 -0400

    updated seeds and added server.js and utils

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Events } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const eventData = require('./eventData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const event of eventData) {
+    await Events.create({
+      ...event,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
