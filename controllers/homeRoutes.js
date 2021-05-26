60da1b02bcc5df2b88c38798c46226de18a8d9e6
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-05-25 21:43:02 -0400

    edited all the files and corrected routes and controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const {Events, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Events, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all events and JOIN with user data
     const eventData = await Events.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -18,31 +18,31 @@ router.get('/', async (req, res) => {
     const events = eventData.map((event) => event.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      events, 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      events,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/event/:id', async (req, res) => {
+router.get("/event/:id", async (req, res) => {
   try {
     const eventData = await Events.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
     const events = eventData.get({ plain: true });
 
-    res.render('events', {
+    res.render("events", {
       ...events,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -50,42 +50,23 @@ router.get('/event/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
 
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-
-router.get('/signup', (req, res) => {
-  if(req.session.logged_in) {
-      res.redirect('/');
-      return;
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
   }
-  res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,2 @@
 DROP DATABASE IF EXISTS splitter_db;
 CREATE DATABASE splitter_db;
-USE splitter_db;
